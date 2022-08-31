471089af9121a60a1efe13c428c274bf10f593af
Luka G <gostencnikluka@proton.me>
2022-08-31 12:15:53 +0200

    Update rofi-pass.sh

--- rofi-pass.sh
+++ rofi-pass.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
+clip_clear=40
+
+
 cd "$HOME"/.password-store || exit 0
 pass_entries=$(find ./* -type f | cut -c 3- | awk -F ".gpg" '{print $1}')
-
-#pes1=$("Insert new password""$pass_entries")
-
-
-pass_entry=$(echo "$pass_entries" | rofi -dmenu -i -p "pass❯") || exit 0
+pass_entry=$(echo -e "$pass_entries" | rofi -dmenu -i -p "pass❯") || exit 0
 pe1=$(pass "$pass_entry")
 line=$(printf '%s\n' "${pe1}" | rofi -dmenu -p "Enter to copy❯")
 printf '%s' "${line}" | xclip -sel clip
-
-
+sleep $clip_clear; printf '%s' "" | xclip -sel clip 
