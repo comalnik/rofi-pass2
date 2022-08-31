d05ffb68ba599223d724dd71985b2c77d511f5f9
Luka G <gostencnikluka@proton.me>
2022-08-31 11:18:01 +0200

    Update rofi-pass.sh

--- rofi-pass.sh
+++ rofi-pass.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
-
 cd "$HOME"/.password-store || exit 0
 pass_entries=$(find ./* -type f | cut -c 3- | awk -F ".gpg" '{print $1}')
 
+#pes1=$("Insert new password""$pass_entries")
 
 
-pass_entry=$(echo "$pass_entries" | rofi -dmenu -i -p "pass") || exit 0
+pass_entry=$(echo "$pass_entries" | rofi -dmenu -i -p "pass❯") || exit 0
 pe1=$(pass "$pass_entry")
+line=$(printf '%s\n' "${pe1}" | rofi -dmenu -p "Enter to copy❯")
+printf '%s' "${line}" | xclip -sel clip
 
-
-
-
-#pe2=$(echo "$pe1" | rofi -dmenu -i -p) || exit 0
-
-bla=$(printf '%s\n' "${pe1}" | rofi -dmenu -mesg "Enter: Copy entry to clipboard" -p "> ")
-
-printf '%s' "${pe1}" | xclip -sel clip
-
-
-
-
-#xdotool type "$(pass show "$pass_entry")"
 
